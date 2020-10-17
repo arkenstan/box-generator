456158fcbae7fa415fb7d59b1905e3b84b34e7f9
arkenstan <anshulwagadre@gmail.com>
2020-10-18 01:23:00 +0530

    added component for rectangle

---
+++ src/app/components/box/box.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Size, BoxPositionData, BoxMoveEvent } from '../../models';
+
+@Component({
+  selector: '[rectBox]',
+  templateUrl: './box.component.html',
+  styleUrls: ['./box.component.scss'],
+})
+export class BoxComponent implements OnInit {
+  @Input('size') size: Size;
+  @Input('position') position: BoxPositionData;
+  @Input('selected') selected: Boolean;
+
+  @Output('focused') focused = new EventEmitter<number>();
+  @Output('blurred') blurred = new EventEmitter<number>();
+  @Output('moved') moved = new EventEmitter<BoxMoveEvent>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onFocus() {
+    this.focused.emit(this.position.boxId);
+  }
+  onBlur() {
+    this.blurred.emit(this.position.boxId);
+  }
+  onMove(event: any) {
+    const { key } = event;
+    this.moved.emit({ key });
+  }
+}
